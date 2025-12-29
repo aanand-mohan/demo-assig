647ee7157e10be2c0069f8dbc4645204aae5bcf6
DEVA SINGH RAJPUT <anandmoha.7970@gmail.com>
2025-12-29 15:44:45 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://demo-assig-4x41.vercel.app/',
         changeOrigin: true
       }
     }
